8c52a3ed74af792805c74dbaedaaef4174389e6c
Raphael Dumdum <135025506+SpokIee@users.noreply.github.com>
2025-12-10 18:35:18 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function replaceAll() {
+    let str = document.getElementById("inputText").value;
+
+    let strResult = str.replaceAll(" ", "");
+    document.getElementById("textResult").innerHTML = strResult;
+}
+
+function countCharacters() {
+    let str = document.getElementById("inputLetter").value;
+    let trimmedStr = str.trim();
+    let charCount = trimmedStr.length;
+            
+    document.getElementById("cntResult").innerHTML = charCount;
+}
+
+/*
+
+Author: Raphael Miguel P. Dumdum
+Date Created: December 12, 2025
+
+*/
